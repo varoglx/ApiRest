37844e238679a9ad4aab7baa3e303778b85069dd
varoglx <114885048+varoglx@users.noreply.github.com>
2023-11-08 20:40:01 -0300

    s
    s

---
+++ app/db - copia.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Crea la instancia de SQLAlchemy
+db = SQLAlchemy()
+
+# Configura la base de datos usando la instancia db
+def configure_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/REST'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+# Otras configuraciones de la base de datos, como modelos y vínculos, pueden ir aquí
